2690e92eb0e48aaa2bf18c0e1a8f8570093250d1
tarik irhboula <tarik.irhboula@gmail.com>
2019-09-19 15:33:50 +0200

    refactor to use data directory + README+

--- storify/__init__.py
+++ storify/__init__.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
 from storify.interfaces.repl import StoreREPL
 from storify.store import Store
+
+ROOT_DIR = Path(__file__).parents[1]
+DATA_DIR = ROOT_DIR / 'data'
+DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 
 def main():
